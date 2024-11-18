ae0c25785a92a943794be2c0b272b384738b0f50
Moataz0000 <moatazfawzy211@gmail.com>
2024-11-19 00:33:04 +0200

    v.4

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import sign_up, sign_out
+from .views import sign_up, sign_out, sign_in
 
 
 app_name = 'accounts'
@@ -8,6 +8,7 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('sign-up/', sign_up, name='sign_up'),
-    path('sign-out/', sign_out, name='sign_out')
+    path('sign-out/', sign_out, name='sign_out'),
+    path('sign-in/', sign_in, name='sign_in')
 
 ]
